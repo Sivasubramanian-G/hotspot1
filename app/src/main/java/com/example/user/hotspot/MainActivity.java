953965ab78867e16765edb188a75dc2d2863c5b4
Sivasubramanian-G <sivasubramanian5.2000@gmail.com>
2018-08-09 17:03:04 +0530

    hot090818

--- app/src/main/java/com/example/user/hotspot/MainActivity.java
+++ app/src/main/java/com/example/user/hotspot/MainActivity.java
@@ -4,13 +4,25 @@ import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                turnOnOffHotspot(MainActivity.this,false);
+            }
+        });
 
     }
 
